62ecc18a0fc66abd02573663d0cf0f5c4a44fed5
Jacob Feenstra <jacob.d.feenstra@gmail.com>
2026-01-12 12:10:51 -0800

    default rust analyzer LSP with kickstart keybindings

--- init.lua
+++ init.lua
@@ -681,7 +681,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        --
+        rust_analyzer = {},
 
         lua_ls = {
           -- cmd = { ... },
